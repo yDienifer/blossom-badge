a568fc9c0650f0d21ce776d80159414efd87f85c
yDienifer <dienifersiqueira.dev@gmail.com>
2025-02-22 09:27:32 -0300

    :sparkles: feat: Adiciona estrutura inicial do formulário com campo de nome

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const app = document.getElementById("app");
 const startApp = () => {
   const content = `
   <form id="form">
+
     <div class="form-group">
       <label>Como podemos te chamar? &#x1F338;&#x2728;</label>
       <input type="text" name="user-name" placeholder="Digite seu nome aqui..." required />
     </div>
-    <!-- Outros campos serão adicionados nas próximas etapas -->
+    
   </form>
   `;
 
